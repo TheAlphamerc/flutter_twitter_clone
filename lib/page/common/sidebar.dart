7c49fd7f012beee9a4c5471d47ee5969aee28b09
Sonu Sharma <sonu.sharma045@gmail.com>
2020-03-09 19:33:14 +0530

    sidebar code refactor

--- lib/page/common/sidebar.dart
+++ lib/page/common/sidebar.dart
@@ -7,9 +7,10 @@ import 'package:flutter_twitter_clone/widgets/newWidget/customUrlText.dart';
 import 'package:provider/provider.dart';
 
 class SidebarMenu extends StatefulWidget {
-  final GlobalKey<ScaffoldState> scaffoldKey ;
+  final GlobalKey<ScaffoldState> scaffoldKey;
 
-  const SidebarMenu({Key key, this.scaffoldKey}) : super(key: key);//= new GlobalKey<ScaffoldState>();
+  const SidebarMenu({Key key, this.scaffoldKey})
+      : super(key: key); //= new GlobalKey<ScaffoldState>();
   _SidebarMenuState createState() => _SidebarMenuState();
 }
 
@@ -18,44 +19,51 @@ class _SidebarMenuState extends State<SidebarMenu> {
 
   Widget _menuHeader() {
     final state = Provider.of<AuthState>(context);
-    if(state.userModel == null){
+    if (state.userModel == null) {
       return customInkWell(
-          context: context, 
-          onPressed: (){
-              //  Navigator.of(context).pushNamed('/signIn');
-            },
-          child: ConstrainedBox(
-            constraints: BoxConstraints(minWidth: 200,minHeight: 100),
-            child: Center(child: Text('Login to continue',style: onPrimaryTitleText,),)
-          ),
-        );
-    }
-   else{
-     return Center(
-       child: Column(
-         crossAxisAlignment: CrossAxisAlignment.start,
-         mainAxisAlignment: MainAxisAlignment.center,
-         children: <Widget>[
-            Container(
-                height: 56,
-                width: 56,
-                margin:EdgeInsets.only(left:17,top:10),
-                decoration: BoxDecoration(
-                  border: Border.all(color:Colors.white,width:2),
-                    borderRadius: BorderRadius.circular(28),
-                    image: DecorationImage(image: customAdvanceNetworkImage(state.userModel.profilePic ?? dummyProfilePic,),fit:BoxFit.cover)
-                )
+        context: context,
+        onPressed: () {
+          //  Navigator.of(context).pushNamed('/signIn');
+        },
+        child: ConstrainedBox(
+            constraints: BoxConstraints(minWidth: 200, minHeight: 100),
+            child: Center(
+              child: Text(
+                'Login to continue',
+                style: onPrimaryTitleText,
               ),
-            ListTile(
-              onTap: (){
-                Navigator.pop(context);
-                Navigator.of(context).pushNamed('/ProfilePage');
-              },
-                title:Row(
+            )),
+      );
+    } else {
+      return Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+              height: 56,
+              width: 56,
+              margin: EdgeInsets.only(left: 17, top: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 2),
+                  borderRadius: BorderRadius.circular(28),
+                  image: DecorationImage(
+                      image: customAdvanceNetworkImage(
+                        state.userModel.profilePic ?? dummyProfilePic,
+                      ),
+                      fit: BoxFit.cover))),
+          ListTile(
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.of(context).pushNamed('/ProfilePage');
+            },
+            title: Row(
               children: <Widget>[
                 UrlText(
-                  text: state.userModel.displayName ?? state.userModel.email.split('.')[0],
-                  style: onPrimaryTitleText.copyWith(color: Colors.black,fontSize: 20),
+                  text: state.userModel.displayName ??
+                      state.userModel.email.split('.')[0],
+                  style: onPrimaryTitleText.copyWith(
+                      color: Colors.black, fontSize: 20),
                 ),
                 SizedBox(
                   width: 3,
@@ -72,77 +80,115 @@ class _SidebarMenuState extends State<SidebarMenu> {
                       ),
               ],
             ),
-              //    customText(
-              //   state.userModel.displayName ?? state.userModel.email.split('.')[0],
-              //   style: onPrimaryTitleText.copyWith(color: Colors.black,fontSize: 20),
-              // ),
-              subtitle: customText(
-                state.userModel.userName,
-                style: onPrimarySubTitleText.copyWith(color: Colors.black54,fontSize: 15),
-              ),
-              trailing: customIcon(context,icon:AppIcon.arrowDown, iconColor: AppColor.primary, paddingIcon: 20),
+            //    customText(
+            //   state.userModel.displayName ?? state.userModel.email.split('.')[0],
+            //   style: onPrimaryTitleText.copyWith(color: Colors.black,fontSize: 20),
+            // ),
+            subtitle: customText(
+              state.userModel.userName,
+              style: onPrimarySubTitleText.copyWith(
+                  color: Colors.black54, fontSize: 15),
             ),
-             Container(
+            trailing: customIcon(context,
+                icon: AppIcon.arrowDown,
+                iconColor: AppColor.primary,
+                paddingIcon: 20),
+          ),
+          Container(
               alignment: Alignment.center,
-              child:Row(
+              child: Row(
                 children: <Widget>[
-                SizedBox(width: 17,),
-                customText('${state.userModel.followers ?? 0 }',style:TextStyle(fontWeight: FontWeight.bold,fontSize:17)),
-                customText(' Followors',style:TextStyle(color: Colors.black54,fontSize:17)),
-                SizedBox(width: 10,),
-                customText('${state.userModel.following ?? 0 }'?? '0',style:TextStyle(fontWeight: FontWeight.bold,fontSize:17)),
-                customText(' Following',style:TextStyle(color: Colors.black54,fontSize:17)),
-              ],)
-            )
-         ],
-       )
-     );
-   }
- }
- ListTile _menuListRowButton(String title,{Function onPressed,int icon, bool isEnable = false}) {
+                  SizedBox(
+                    width: 17,
+                  ),
+                  customText('${state.userModel.followers ?? 0}',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 17)),
+                  customText(' Followors',
+                      style: TextStyle(color: Colors.black54, fontSize: 17)),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  customText('${state.userModel.following ?? 0}' ?? '0',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 17)),
+                  customText(' Following',
+                      style: TextStyle(color: Colors.black54, fontSize: 17)),
+                ],
+              ))
+        ],
+      ));
+    }
+  }
+
+  ListTile _menuListRowButton(String title,
+      {Function onPressed, int icon, bool isEnable = false}) {
     return ListTile(
-       onTap: (){
-            if(onPressed != null){
-              onPressed();
-            }
-          },
-        leading: Padding(
-          padding: EdgeInsets.only(top: 5),
-          child:icon == null ? SizedBox() : customIcon(context,icon: icon, size: 25 ,iconColor: isEnable ? AppColor.darkGrey : AppColor.lightGrey),
-        ),
-        title:customText(title,style: TextStyle(fontSize: 22, color: isEnable ? AppColor.secondary : AppColor.lightGrey)),
+      onTap: () {
+        if (onPressed != null) {
+          onPressed();
+        }
+      },
+      leading: Padding(
+        padding: EdgeInsets.only(top: 5),
+        child: icon == null
+            ? SizedBox()
+            : customIcon(context,
+                icon: icon,
+                size: 25,
+                iconColor: isEnable ? AppColor.darkGrey : AppColor.lightGrey),
+      ),
+      title: customText(title,
+          style: TextStyle(
+              fontSize: 22,
+              color: isEnable ? AppColor.secondary : AppColor.lightGrey)),
     );
   }
- void _logOut(){
-   final state = Provider.of<AuthState>(context);
-   state.logoutCallback();
- }
-dispose() {
-  super.dispose();
-}
+
+  void _logOut() {
+    final state = Provider.of<AuthState>(context);
+    state.logoutCallback();
+  }
+
+  dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: SafeArea(
         child: Column(
-            children: <Widget>[
-              Container(
-                height: 160,
-                child: _menuHeader(),
-              ),
+          children: <Widget>[
+            Container(
+              height: 160,
+              child: _menuHeader(),
+            ),
             Divider(),
-            _menuListRowButton('Profile',icon: AppIcon.profile,isEnable: true, onPressed: (){Navigator.of(context).pushNamed('/ProfilePage');}),
-            _menuListRowButton('Lists',icon: AppIcon.lists),
-            _menuListRowButton('Bookamrks',icon:AppIcon.bookmark),
-            _menuListRowButton('Moments',icon: AppIcon.moments),
-            _menuListRowButton('Twitter ads',icon: AppIcon.twitterAds),
+            _menuListRowButton(
+              'Profile',
+              icon: AppIcon.profile,
+              isEnable: true,
+              onPressed: () {
+                Navigator.of(context).pushNamed('/ProfilePage');
+              },
+            ),
+            _menuListRowButton('Lists', icon: AppIcon.lists),
+            _menuListRowButton('Bookamrks', icon: AppIcon.bookmark),
+            _menuListRowButton('Moments', icon: AppIcon.moments),
+            _menuListRowButton('Twitter ads', icon: AppIcon.twitterAds),
             Divider(),
-            _menuListRowButton('Settings and privacy',),
-            _menuListRowButton('Help Center',),
+            _menuListRowButton(
+              'Settings and privacy',
+            ),
+            _menuListRowButton(
+              'Help Center',
+            ),
             Divider(),
-            _menuListRowButton('Logout',onPressed: _logOut,isEnable: true)
+            _menuListRowButton('Logout', onPressed: _logOut, isEnable: true)
           ],
-        ),  
-    ));
+        ),
+      ),
+    );
   }
 }
