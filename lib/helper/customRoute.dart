a48868d4c6bf894064c82940446f32c3039c0efb
Sonu Sharma <sonu.sharma045@gmail.com>
2020-03-11 12:01:19 +0530

    slide left custom page route added

--- lib/helper/customRoute.dart
+++ lib/helper/customRoute.dart
@@ -15,3 +15,22 @@ class CustomRoute<T> extends MaterialPageRoute<T> {
     );
   }
 }
+
+class SlideLeftRoute<T> extends MaterialPageRoute<T> {
+  SlideLeftRoute({WidgetBuilder builder, RouteSettings settings})
+      : super(builder: builder, settings: settings);
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    if (settings.isInitialRoute) {
+      return child;
+    }
+    return SlideTransition(
+      position: new Tween<Offset>(
+        begin: const Offset(1.0, 0.0),
+        end: Offset.zero,
+      ).animate(CurvedAnimation(parent: animation, curve: Curves.easeIn)),
+      child: child,
+    );
+  }
+}
