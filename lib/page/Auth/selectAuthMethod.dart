9bd799b99cf84b6c0e1ffa5ef3ff5b200a1879d9
Sonu Sharma <sonu.sharma045@gmail.com>
2020-03-15 17:22:17 +0530

    Implement autologin

--- lib/page/Auth/selectAuthMethod.dart
+++ lib/page/Auth/selectAuthMethod.dart
@@ -6,32 +6,31 @@ import 'package:provider/provider.dart';
 import '../homePage.dart';
 import 'signin.dart';
 
-class SelectAuthMethod extends StatefulWidget{
+class SelectAuthMethod extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _SelectAuthMethodState();
-  
 }
+
 class _SelectAuthMethodState extends State<SelectAuthMethod> {
   @override
   void initState() {
-     var state = Provider.of<AuthState>(context,listen: false);
-     state.authStatus = AuthStatus.NOT_DETERMINED;
-    //  state.getCurrentUser();
+    var state = Provider.of<AuthState>(context, listen: false);
+    state.authStatus = AuthStatus.NOT_DETERMINED;
+    state.getCurrentUser();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     var state = Provider.of<AuthState>(context);
     return Scaffold(
-       body:
-      //  state.authStatus == AuthStatus.NOT_DETERMINED ? 
-      //  SignIn() :
-       state.authStatus == AuthStatus.NOT_DETERMINED?
-       SignIn(loginCallback: state.getCurrentUser,) 
-       : state.authStatus == AuthStatus.NOT_LOGGED_IN ?
-         Signup(loginCallback: state.getCurrentUser)  
-       : HomePage()  
-       
+      body: state.authStatus == AuthStatus.NOT_DETERMINED
+          ? SignIn(
+              loginCallback: state.getCurrentUser,
+            )
+          : state.authStatus == AuthStatus.NOT_LOGGED_IN
+              ? Signup(loginCallback: state.getCurrentUser)
+              : HomePage(),
     );
   }
 }
