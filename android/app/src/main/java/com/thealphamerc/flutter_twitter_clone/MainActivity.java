3a63bb9ff7992f0336c47592cac7038665e3be80
Mario de la Cuadra Izquierdo <mcuadrai@gmail.com>
2020-11-05 17:30:59 -0300

    Cambiando proyecto Android deprecado a Android embedding

--- android/app/src/main/java/com/thealphamerc/flutter_twitter_clone/MainActivity.java
+++ android/app/src/main/java/com/thealphamerc/flutter_twitter_clone/MainActivity.java
@@ -1,13 +1,12 @@
 package com.thealphamerc.flutter_twitter_clone;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity
+
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+  // @Override
+  // protected void onCreate(Bundle savedInstanceState) {
+  //  super.onCreate(savedInstanceState);
+  //  GeneratedPluginRegistrant.registerWith(this);
+ //  }
 }
