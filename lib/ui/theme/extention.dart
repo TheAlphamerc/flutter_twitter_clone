0ec1e72197860358ac266c8cb34e201f173998da
Sonu Sharma <sonu.sharma045@gmail.com>
2021-05-07 08:58:34 +0530

    :bug: (fix) Getting error in tweet detail Refrence - > https://github.com/TheAlphamerc/flutter_twitter_clone/issues/163

--- lib/ui/theme/extention.dart
+++ lib/ui/theme/extention.dart
@@ -5,7 +5,7 @@ extension SizeHelper on BuildContext {
   double get height => MediaQuery.of(this).size.height;
 
   double getDimention(context, double unit) {
-    if (context.width <= 360.0) {
+    if (width <= 360.0) {
       return unit / 1.3;
     } else {
       return unit;
